24fd4ffc8493ad9e7b4286345c5cc5c33c1b9983
marveltechhub-byte <onyemap250@gmail.com>
2026-01-18 12:11:48 +0000

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://de-sons-voice-backend-1.onrender.com",
+  baseURL: "https://de-sons-voice-backend-1.onrender.com/api",
   withCredentials: true, // REQUIRED if using cookies/JWT
 });
 
 export default API;
+
 
